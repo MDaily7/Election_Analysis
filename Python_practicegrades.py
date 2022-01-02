fdaffa786dea27fdf114997da9928a5f7d47811f
MDaily7 <mdaily622@gmail.com>
2022-01-02 11:42:30 -0800

    changed nested if thens to elifs

--- Python_practicegrades.py
+++ Python_practicegrades.py
@@ -1,15 +1,13 @@
 grade = int(input("What is your grade?"))
 if grade >=90:
     print("Your grade is an A")
-else: 
-    if grade >=80:
-        print("Your grade is a B")
-    else:
-        if grade >=70:
-            print("Your grade is a C")
-        else:
-            if grade >=60:
-                print("Your grade is a D")
-            else: 
-                print("Your grade is an F")
-                
+elif grade>=80:
+    print("Your grade is a B")
+elif grade>=70:
+    print("Your grade is a C")
+elif grade>=60:
+    print("Your grade is a D")
+else:
+    print("Your grade is an F")
+
+    
