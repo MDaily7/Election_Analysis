dc88b63cb8fad3a551e23bbd928dab9dacda7b83
MDaily7 <mdaily622@gmail.com>
2022-01-02 11:33:35 -0800

    pythonpracticegrades

---
+++ Python_practicegrades.py
@@ -0,0 +1,15 @@
+grade = int(input("What is your grade?"))
+if grade >=90:
+    print("Your grade is an A")
+else: 
+    if grade >=80:
+        print("Your grade is a B")
+    else:
+        if grade >=70:
+            print("Your grade is a C")
+        else:
+            if grade >=60:
+                print("Your grade is a D")
+            else: 
+                print("Your grade is an F")
+                
