6a5504f9c1197765ee8b286d0a9bb1169d718b54
MDaily7 <mdaily622@gmail.com>
2022-01-03 10:46:45 -0800

    modified PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -1,13 +1,14 @@
-#Assign a variable for the file to load and a path
-file_to_load = 'Resources/election_results.csv'
-# Open the election results and read the file.
+#Add Dependencies
+import csv
+import os
+#Assign Variable to load file from a path
+file_to_load = os.path.join("Resources", "election_results.csv")
+#Assign Variable to save a file to a path
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+#Open Election Results and Read file
 with open(file_to_load) as election_data:
-    #To do: perform analysis
-    print(election_data)
-#Create a filename variable to a direct path to the file
-file_to_save = 'Analysis/election_analysis.txt'
-#Using a with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    #Write some data to the file.
-    txt_file.write("Counties in the Election\n------------\nArapahoe\nDenver\nJefferson")
-
+    file_reader = csv.reader(election_data)
+    #Read and Print Header Row
+    header = next(file_reader)
+    print(header)
+    
